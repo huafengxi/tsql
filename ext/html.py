e17e5884bff70ed816ef5f99cbf9359edba7d099
huafengxi <huafengxi@gmail.com>
2021-10-09 23:57:21 +0800

    add ParamLink and ImgLink

--- ext/html.py
+++ ext/html.py
@@ -3,7 +3,14 @@ HtmlLink(url, text=None)
 '''
 def HtmlLink(url, text=None):
     return '<a href="%s" target="_blank">%s<a>'%(url, text)
+def ParamLink(tpl, arg):
+    return HtmlLink(tpl % arg, arg)
+
+def ImgLink(tpl, arg):
+    return '<img src="%s" alt="%s"/>'%(tpl % arg, arg)
 
 def reg_sqlite_ext(self, conn):
     self.reg_func(conn, HtmlLink, 1)
     self.reg_func(conn, HtmlLink, 2)
+    self.reg_func(conn, ParamLink, 2)
+    self.reg_func(conn, ImgLink, 2)
